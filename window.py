96a5179b3efa2f82b17d099a6c1744e7abd8854e
CaseyWav <casey@waverka.com>
2022-09-30 22:16:46 -0400

    Fixed reshape issue and file explorer button
    Will need to look into placing of buttons and labels.

--- window.py
+++ window.py
@@ -3,14 +3,15 @@ from tkinter import ttk
 from tkinter import filedialog
 from tkinter.messagebox import showinfo
 
-
 class App(tk.Tk):
   def __init__(self):
     super().__init__()
 
     # configure the root window
     self.title('Data Visualizer')
-    self.attributes('-fullscreen', True)
+    width = self.winfo_screenwidth()
+    height = self.winfo_screenheight()
+    self.geometry("%dx%d" % (width, height))
 
     # label
     self.label = ttk.Label(self, text='Hello, Tkinter!')
@@ -21,28 +22,23 @@ class App(tk.Tk):
     self.button['command'] = self.button_clicked
     self.button.pack()
 
-    # close program button
-    exit_button = ttk.Button(self, text="X", command=self.destroy)
-    exit_button.place(relx=.982, rely=0.01, anchor="c")
-
-    
-    
     label_file_explorer = ttk.Label(self, text = "File Explorer using Tkinter")
     label_file_explorer.place(relx=.75,rely=.05, anchor="c")
-    button_explore = ttk.Button(self, text = "Browse Files", command = self.browseFiles())
+    self.button['command'] = self.button_clicked
+    button_explore = ttk.Button(self, text = "Browse Files", command = (lambda: self.browseFiles(label_file_explorer)))
     
+    button_explore.place(relx=.75,rely=.07, anchor="c")
     
 
   def button_clicked(self):
     showinfo(title='Information', message='Hello, Tkinter!')
 
-  def browseFiles(label_file_explorer):
+  def browseFiles(self, label_file_explorer):
     filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("Text files","*.txt*"),("all files","*.*")))
       
     # Change label contents
     label_file_explorer.configure(text="File Opened: "+filename)
     
-
 
 if __name__ == "__main__":
   app = App()
