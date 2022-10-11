9632a09bce113cc3c244c0280bf40d3dcc8146e3
CaseyWav <casey@waverka.com>
2022-10-10 21:49:49 -0400

    Started the foundation to creating a frame within a frame

--- window.py
+++ window.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import *
 from tkinter import OptionMenu, ttk
 from tkinter import filedialog
+from turtle import bgcolor
 from data_analysis_ddl import analysis_options
 from tkinter.messagebox import showinfo
 
@@ -11,24 +12,27 @@ class App(tk.Tk):
 
     # configure the root window
     self.title('Data Visualizer')
-    width = self.winfo_screenwidth()
-    height = self.winfo_screenheight()
-    self.geometry("%dx%d" % (width, height))
+    screen_width = self.winfo_screenwidth()
+    screen_height = self.winfo_screenheight()
+    self.geometry("%dx%d" % (screen_width, screen_height))
+
+    frame1 = tk.Frame(self,width=(screen_width*.5),height=(screen_height*.5),bg="blue")
+    frame1.place(relx=.5,rely=.5)
 
     # label
     self.label = ttk.Label(self, text='Hello, Tkinter!')
-    self.label.grid(row=0,column=0,columnspan=5)
+    self.label.place(relx=.5,rely=.1)
 
     # button
     self.button = ttk.Button(self, text='Click Me')
     self.button['command'] = self.button_clicked
-    self.button.grid(row=1,column=3,ipadx=.5,ipady=.5)
+    self.button.place(relx=.8,rely=.2)
     label_file_explorer = ttk.Label(self, text = "File Explorer using Tkinter")
     label_file_explorer.place(relx=.75,rely=.05, anchor="c")
     self.button['command'] = self.button_clicked
     button_explore = ttk.Button(self, text = "Browse Files", command = (lambda: self.browseFiles(label_file_explorer)))
     
-    button_explore.grid(row=3,column=4)
+    button_explore.place(relx=.75,rely=.1)
     
     clicked = StringVar(self)
     clicked.set("Select an analysis option")
@@ -41,7 +45,7 @@ class App(tk.Tk):
     
 
   def browseFiles(self, label_file_explorer):
-    filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("Text files","*.txt*"),("all files","*.*")))
+    filename = filedialog.askopenfilename()
       
     # Change label contents
     label_file_explorer.configure(text="File Opened: "+filename)
