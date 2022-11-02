33e4375c0e506d5e30ea57be0ac941e08bc5d826
CaseyWav <casey@waverka.com>
2022-11-02 19:36:35 -0400

    will start even simpler for reading and displaying data

--- file_reader.py
+++ file_reader.py
@@ -1,26 +1,7 @@
-from matplotlib import pyplot as plt
-import csv
-import numpy as np
-
-filename = 'baselinedata_corrections.csv'
-
-def readFile(filename):
-    f = open(filename, encoding='UTF8')
-    d13c = []
-    d15n = []
-  
-    with open(filename,'r') as csvfile:
-        lines = csv.reader(csvfile, delimiter=',')
-        for row in lines:
-            d13c.append(row[0])
-            d15n.append(int(row[1]))
-  
-    plt.scatter(d13c, d15n, color = 'g',s = 100)
-    plt.xticks(rotation = 25)
-    plt.xlabel('Carbon')
-    plt.ylabel('Nitrogen')
-    plt.title('Food chart', fontsize = 20)
-  
-    plt.show()
-
-    f.close()
+import pandas
+ 
+# reading the CSV file
+csvFile = pandas.read_csv('baselinedata_corrections.csv')
+ 
+# displaying the contents of the CSV file
+print(csvFile)
