16e6f44123e8f02224702300b489477e51106623
CaseyWav <casey@waverka.com>
2022-11-15 18:25:23 -0500

    got the file reading but not correctly

--- file_reader.py
+++ file_reader.py
@@ -5,11 +5,11 @@ import csv
 d13c = []
 d15n = []
   
-with open('D:\Programming\Data Analysis Program\Data-Viusualizer\ baselinedata_corrections.csv','r') as csvfile:
-    lines = csv.reader('csvfile', delimiter=',')
+with open('Data-Visualizer/baselinedata_corrections.csv','r') as csvfile:
+    lines = csv.reader(csvfile, delimiter=',')
     for row in lines:
         d13c.append(row[0])
-        d15n.append(int(row[1]))
+        d15n.append(row[1])
   
 plt.scatter(d13c, d15n, color = 'g',s = 100)
 plt.xticks(rotation = 25)
@@ -18,5 +18,3 @@ plt.ylabel('Nitrogen')
 plt.title('Food chart', fontsize = 20)
   
 plt.show()
-
-f.close()
